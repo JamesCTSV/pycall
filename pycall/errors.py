129c4e6204eb28030cf1c3175b5d297757668218
jamesCTSV <fadects@gmail.com>
2019-08-20 13:17:45 +0200

    Revert "added remote_spool function"
    This reverts commit 4f4111b8

--- pycall/errors.py
+++ pycall/errors.py
@@ -1,6 +1,8 @@
 """Custom error classes for signaling issues."""
 
+
 from sys import version_info
+
 
 if version_info < (3, 0, 0):
     from exceptions import Exception
@@ -22,10 +24,6 @@ class NoSpoolPermissionError(PycallError):
 
 class NoUserError(PycallError):
     """User does not exist."""
-
-
-class ParamikoError(PycallError):
-    """ Something went wrong starting SSH connection."""
 
 
 class NoUserPermissionError(PycallError):
